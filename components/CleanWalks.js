9f912a71bd6310778654ba9f03a1d3f0cbbbc834
Erwan Rivoalen <erwan.rivoalen422@orange.fr>
2023-04-07 11:38:14 +0200

    cleanwalk screen sans itineraire

--- components/CleanWalks.js
+++ components/CleanWalks.js
@@ -1,113 +1,99 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
-import { supabase } from './supabase';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {supabase} from './supabase';
 
-const CleanWalksScreen = () => {
+const CleanWalks = () => {
     const [cleanWalks, setCleanWalks] = useState([]);
-    const [selectedCleanWalk, setSelectedCleanWalk] = useState({ points: [] });
+    const [selectedCleanWalk, setSelectedCleanWalk] = useState(null);
+    const [cleanWalkPoints, setCleanWalkPoints] = useState([]);
 
     useEffect(() => {
-        const fetchCleanWalks = async () => {
-            const { data, error } = await supabase
+        async function fetchCleanWalks() {
+            let { data: cleanWalks, error } = await supabase
                 .from('cleanWalks')
-                .select('id, objects_id');
+                .select('*');
             if (error) console.log('Error fetching clean walks:', error);
-            else setCleanWalks(data);
-        };
+            else setCleanWalks(cleanWalks);
+            console.log('Ok');
+        }
         fetchCleanWalks();
     }, []);
 
-    const fetchCleanWalkPoints = async (pointsIds) => {
-        const { data, error } = await supabase
-            .from('objects')
-            .select('latitude, longitude')
-            .in('id', pointsIds);
-        if (error) {
-            console.log('Error fetching clean walk points:', error);
-        } else {
-            const points = data
-                .filter((point) => point.latitude && point.longitude)
-                .map((point) => ({
-                    latitude: point.latitude,
-                    longitude: point.longitude,
-                }));
-            setSelectedCleanWalk({ points });
+    const handleViewItinerary = async (cleanWalk) => {
+        setSelectedCleanWalk(cleanWalk);
+        let points = [];
+
+        for (let i = 0; i < cleanWalk.objects_id.length; i++) {
+            let { data: objects, error } = await supabase
+                .from('objects')
+                .select('latitude, longitude')
+                .eq('id', cleanWalk.objects_id[i]);
+            if (error) console.log('Error fetching points:', error);
+            else points.push(objects[0]);
         }
+        setCleanWalkPoints(points);
     };
-    const clearSelectedCleanWalk = () => {
+
+    const handleBackToList = () => {
         setSelectedCleanWalk(null);
+        setCleanWalkPoints([]);
     };
 
-    return (
-        <View style={styles.container}>
-            {selectedCleanWalk ? (
-                <View style={styles.mapContainer}>
-                    <MapView
-                        style={styles.map}
-                        initialRegion={{
-                            latitude: selectedCleanWalk.points[0].latitude,
-                            longitude: selectedCleanWalk.points[0].longitude,
-                            latitudeDelta: 0.01,
-                            longitudeDelta: 0.01,
-                        }}
-                    >
-                        {selectedCleanWalk.points && selectedCleanWalk.points.length > 0 && (
-                            <Polyline
-                                coordinates={selectedCleanWalk.points.map((point) => ({
-                                    latitude: point.latitude,
-                                    longitude: point.longitude,
-                                }))}
-                                strokeColor="#000"
-                                strokeWidth={2}
-                            />
-                        )}
-                    </MapView>
-                    <Button title="Back" onPress={clearSelectedCleanWalk} />
-                </View>
-            ) : (
-                <FlatList
-                    data={cleanWalks}
-                    renderItem={({ item }) => (
-                        <View style={styles.cleanWalkContainer}>
-                            <Text style={styles.cleanWalkTitle}>Clean Walk {item.id}</Text>
-                            <Button
-                                title="View Itinerary"
-                                onPress={() => fetchCleanWalkPoints(item.points)}
-                            />
-                        </View>
-                    )}
-                    keyExtractor={(item) => item.id.toString()}
-                />
-            )}
-        </View>
-    );
+    if (selectedCleanWalk) {
+        return (
+            <View style={{ flex: 1 }}>
+                <MapView style={{ flex: 1 }} initialRegion={{
+                    latitude: 48.8666,
+                    longitude: 2.33333,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}>
+                    {cleanWalkPoints.map((point, index) => (
+                        <Marker key={index} coordinate={{ latitude: point.latitude, longitude: point.longitude }} />
+                    ))}
+                </MapView>
+                <Button title="Back to List" onPress={handleBackToList} />
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.header}>Clean Walks</Text>
+                {cleanWalks.map((cleanWalk) => (
+                    <View key={cleanWalk.id} style={styles.item}>
+                        <Text style={styles.title}>{`Clean Walk ${cleanWalk.id}`}</Text>
+                        <Button
+                            title="View Itinerary"
+                            onPress={() => handleViewItinerary(cleanWalk)}
+                        />
+                    </View>
+                ))}
+            </View>
+        );
+    }
 };
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20,
     },
-    cleanWalkContainer: {
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ddd',
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
     },
-    cleanWalkTitle: {
-        fontSize: 18,
+    item: {
+        marginBottom: 20,
     },
-    mapContainer: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 5,
     },
 });
 
-export default CleanWalksScreen;
+export default CleanWalks;
