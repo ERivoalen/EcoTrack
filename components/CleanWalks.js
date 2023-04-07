56b102255d41ce07c6fbe26ea792761671306fcb
Erwan Rivoalen <erwan.rivoalen422@orange.fr>
2023-04-07 12:43:48 +0200

    feat: cleanwalk paths

--- components/CleanWalks.js
+++ components/CleanWalks.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import {supabase} from './supabase';
+import MapView, { Marker, Polyline, Callout } from 'react-native-maps';
+import { supabase } from './supabase';
+import axios from 'axios';
+
+const API_KEY = '5b3ce3597851110001cf62488c1e79adf789490eb72896646a9c095e';
 
 const CleanWalks = () => {
     const [cleanWalks, setCleanWalks] = useState([]);
     const [selectedCleanWalk, setSelectedCleanWalk] = useState(null);
     const [cleanWalkPoints, setCleanWalkPoints] = useState([]);
+    const [itinerary, setItinerary] = useState([]);
+    const [itineraryCalculated, setItineraryCalculated] = useState(false);
 
     useEffect(() => {
         async function fetchCleanWalks() {
@@ -20,6 +25,7 @@ const CleanWalks = () => {
         fetchCleanWalks();
     }, []);
 
+
     const handleViewItinerary = async (cleanWalk) => {
         setSelectedCleanWalk(cleanWalk);
         let points = [];
@@ -32,13 +38,70 @@ const CleanWalks = () => {
             if (error) console.log('Error fetching points:', error);
             else points.push(objects[0]);
         }
+        console.log(points)
         setCleanWalkPoints(points);
+        //handleCalculateItinerary();
+        const itineraryCoordinates = [];
+        console.log(cleanWalkPoints);
+        for (let i = 0; i < points.length - 1; i++) {
+            const startPoint = points[i];
+            const endPoint = points[i + 1];
+            console.log('bouvle');
+            const segmentCoordinates = await calculateItinerary(startPoint, endPoint);
+            itineraryCoordinates.push(...segmentCoordinates);
+        }
+
+        const formattedItinerary = itineraryCoordinates.map((coordinate) => ({
+            latitude: coordinate[1],
+            longitude: coordinate[0],
+        }));
+
+        setItinerary(formattedItinerary);
+        setItineraryCalculated(true);
     };
 
     const handleBackToList = () => {
         setSelectedCleanWalk(null);
         setCleanWalkPoints([]);
     };
+
+    const calculateItinerary = async (startPoint, endPoint) => {
+        console.log('Ici');
+        const { data } = await axios.get(
+            `https://api.openrouteservice.org/v2/directions/foot-walking?api_key=${API_KEY}&start=${startPoint.longitude},${startPoint.latitude}&end=${endPoint.longitude},${endPoint.latitude}`
+        );
+        console.log(data);
+
+        return data.features[0].geometry.coordinates;
+    };
+
+    const handleCalculateItinerary = async () => {
+        const itineraryCoordinates = [];
+        console.log(cleanWalkPoints);
+        for (let i = 0; i < cleanWalkPoints.length - 1; i++) {
+            const startPoint = cleanWalkPoints[i];
+            const endPoint = cleanWalkPoints[i + 1];
+            console.log('bouvle');
+            const segmentCoordinates = await calculateItinerary(startPoint, endPoint);
+            itineraryCoordinates.push(...segmentCoordinates);
+        }
+
+        const formattedItinerary = itineraryCoordinates.map((coordinate) => ({
+            latitude: coordinate[1],
+            longitude: coordinate[0],
+        }));
+
+        setItinerary(formattedItinerary);
+        setItineraryCalculated(true);
+    };
+
+    useEffect(() => {
+        if (itineraryCalculated) {
+            // Trigger map re-render when itinerary is calculated
+            setSelectedCleanWalk(selectedCleanWalk);
+        }
+    }, [itineraryCalculated]);
+
 
     if (selectedCleanWalk) {
         return (
@@ -52,7 +115,11 @@ const CleanWalks = () => {
                     {cleanWalkPoints.map((point, index) => (
                         <Marker key={index} coordinate={{ latitude: point.latitude, longitude: point.longitude }} />
                     ))}
+                    {itineraryCalculated && (
+                        <Polyline coordinates={itinerary} strokeColor="#f00" strokeWidth={3} />
+                    )}
                 </MapView>
+
                 <Button title="Back to List" onPress={handleBackToList} />
             </View>
         );
@@ -65,7 +132,7 @@ const CleanWalks = () => {
                         <Text style={styles.title}>{`Clean Walk ${cleanWalk.id}`}</Text>
                         <Button
                             title="View Itinerary"
-                            onPress={() => handleViewItinerary(cleanWalk)}
+                            onPress={() => { handleViewItinerary(cleanWalk) }}
                         />
                     </View>
                 ))}
